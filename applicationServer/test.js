b8b26f026d9d112675b03a6631389a43d14dcd4a
Ubuntu <ubuntu@ip-172-31-12-208.us-east-2.compute.internal>
2020-12-30 00:19:49 +0000

    Adding MySql querying test file

---
+++ applicationServer/test.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	    host: 'localhost',
+	    user: 'root',
+	    password: 'mypass123',
+	    database: 'testDatabase'
+});
+
+con.connect();
+
+var query1 = "SELECT the_time, numPeople FROM Camera_1";
+
+con.query(query1, function(err, result, fields){
+	            if(err) throw err;
+	            //console.log(rows);
+	           // console.log(fields);
+	            console.log(result);
+});
+
+con.end();
